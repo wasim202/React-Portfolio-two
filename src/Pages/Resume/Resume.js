8ae4e654efa878ba90d65276fbea3765c16ac57d
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-10-26 15:38:29 -0600

    resume

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import resume from './Wasim-resume-New.pdf';
+import resume from './Wasim-resume.pdf';
 
 
-
+const style = {
+  object: {
+    height: "700px",
+  },
+};
 
 export default function Resume() {
   return (
-    <div className='container d-flex flex-column min vh-100'>
-    <div>
+    <div className='mt-3 d-flex flex-column min vh-100'>
+      <h1 className=" text-center">Resume</h1>
+      <div className='container'>
+    <ul className='text-center list-group'>
      <a href={resume} download="Wasim Mohammad resume"> Download Resume </a>
-    </div>
-    </div>
+
+     <object className='list-group-item bg-dark' style={style.object} data={resume}></object>
+    </ul>
+  </div>
+ </div>
   )
 }
