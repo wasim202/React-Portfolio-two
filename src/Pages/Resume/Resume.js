1663443386215471f4c6ce8310ce5f6d22225b7d
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-10-27 23:46:08 -0600

    fix resume

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -13,12 +13,12 @@ export default function Resume() {
     <div className='mt-3 d-flex flex-column min vh-100'>
       <h1 className=" text-center">Resume</h1>
       <div className='container'>
-    <ul className='text-center list-group'>
+    <div className='text-center list-group'>
      {/* <a href={resume} download="Wasim Mohammad resume"> Download Resume </a> */}
 
      <object className='list-group-item bg-dark' style={style.object} data={resume}></object>
-    </ul>
+    </div>
   </div>
  </div>
-  )
+  );
 }
