9e18d62805375b0d5c9865cdba8c5aa0acce0ea6
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-10-28 01:05:45 -0600

    fix resume

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import './Resume.css'
 import resume from './Wasim-resume.pdf';
+import resumePic from '../../images/resume.jpeg'
 
 
 const style = {
@@ -13,12 +15,24 @@ export default function Resume() {
     <div className='mt-3 d-flex flex-column min vh-100'>
       <h1 className=" text-center">Resume</h1>
       <div className='container'>
-    <div className='text-center list-group'>
-     {/* <a href={resume} download="Wasim Mohammad resume"> Download Resume </a> */}
-
-     <object className='list-group-item bg-dark' style={style.object} data={resume}></object>
+      <ul className="text-center list-group">
+      <a
+      className="list-group-item bg-dark text-light list-group-item-action"
+       href={resume} download="Wasim Mohammad resume"> Download Resume </a> 
+       <img className='resumeImg' src={resumePic} alt="Wasim's resume"/>
+       </ul>
+      </div>
     </div>
-  </div>
- </div>
-  );
+//-----------------------------------------------------------------------
+//     <div className='mt-3 d-flex flex-column min vh-100'>
+//       <h1 className=" text-center">Resume</h1>
+//       <div className='container'>
+//     <div className='text-center list-group'>
+//      {/* <a href={resume} download="Wasim Mohammad resume"> Download Resume </a> */}
+
+//      <object className='list-group-item bg-dark' style={style.object} data={resume}></object>
+//     </div>
+//   </div>
+//  </div>
+  )
 }
